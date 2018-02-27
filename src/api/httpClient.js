e0750c436b06a01f18d7ef5a1a3f6c96e257a986
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2018-02-27 11:51:55 +0300

    react environment setup

---
+++ src/api/httpClient.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const post = (url = '', data = '', config = {}) => {
+    return axios.post(url, data, config)
+}
+
+const get = (url) => {
+    return axios(url)
+}
+
+const put = (url = '', data = '', config = {}) => {
+    return axios.put(url, data, config)
+}
+
+const del = (url = '', data = '', config = {}) => {
+    return axios.delete(url, config)
+}
